a88d1b99b67b2fef0adadc95779f7f4349fa0697
Ricardo Pinto <1140482@isep.ipp.pt>
2018-12-20 01:27:26 +0000

    motion sensor improvements

--- sensor_array/motion_sensor.py
+++ sensor_array/motion_sensor.py
@@ -1,18 +1,28 @@
 import RPi.GPIO as GPIO
 import time
+import datetime
 
 pin_sensor = 14
+pin_relay = 15
+
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(pin_sensor, GPIO.IN) #PIR
+GPIO.setup(pin_relay, GPIO.OUT) #RELAY
+GPIO.setup(pin_relay, GPIO.LOW) #RELAY off
 
 try:
     time.sleep(2) # to stabilize sensor
     while True:
+        current_time=datetime.datetime.now()
+        print(current_time)
         if GPIO.input(pin_sensor):
             print("Motion Detected...")
-            time.sleep(5) #to avoid multiple detection
-        time.sleep(0.1) #loop delay, should be less than detection delay
+            GPIO.setup(pin_relay, GPIO.LOW) #RELAY off            
+        else:
+            print("nothing...")
+            GPIO.setup(pin_relay, GPIO.HIGH) #RELAY on
+        time.sleep(1) #to avoid multiple detection
 
 except:
     GPIO.cleanup()
