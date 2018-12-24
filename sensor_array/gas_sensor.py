e49539f9f34d1dc8c39ce0e061d9dad5a6929bfb
Ricardo Pinto <1140482@isep.ipp.pt>
2018-12-24 16:02:20 +0000

    http sensor array added, luminosity sensor added but not tested, waiting for sensor arrival

--- sensor_array/gas_sensor.py
+++ sensor_array/gas_sensor.py
@@ -1,25 +1,26 @@
-import RPi.GPIO as GPIO
-import time
-import json
-import datetime
+import RPi.GPIO as GPIO 
+import time 
+import json 
+import datetime 
 
-pino_sensor = 4
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(pino_sensor, GPIO.IN)
+pino_sensor = 4 
 
-try:
-     while True:
-          result = GPIO.input(pino_sensor)
-          detected_flag = (result==0) #detected gas
-          
-          data = {}
-          data['gas_detected'] = detected_flag
-          
-          print(json.dumps(data))
-          time.sleep(1)
-
-except KeyboardInterrupt:          
-     print("Interrompido pelo utilizador!!!")
-
-except:
-    GPIO.cleanup()
+def getValues():
+	
+	try:
+		#while True:
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(pino_sensor, GPIO.IN)
+		result = GPIO.input(pino_sensor)
+		detected_flag = (result==0) #detected gas
+		
+		data = {}
+		data['gas_detected'] = detected_flag
+		return json.dumps(data, indent=4, sort_keys=True)
+       
+	except KeyboardInterrupt as e:
+		print("Interrompido pelo utilizador!!!")
+		return e
+	except Exception as e:
+		GPIO.cleanup()
+		return e
