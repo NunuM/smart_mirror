a5b9b50a276fa64f278730c0db6bf3e61322b20b
Nuno Oliveira <1140460@isep.ipp.pt>
2018-12-29 19:53:13 +0000

    Cross compiling coordinator to raspberry pi

--- coordinator_service/install.sh
+++ coordinator_service/install.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-cp coordinatorARM64 /usr/local/bin/
+cp coordinator /usr/local/bin/
 cp coordinator.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl start coordinator.service
